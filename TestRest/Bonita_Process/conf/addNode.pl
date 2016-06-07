e4bfef0de813e7a200da28583af22718196bd5e4
yassine <yadine.yassine7@gmail.com>
2016-06-07 16:21:11 +0200

    Commit n° 7
    Push de l'organisation et des fichiers BAR

---
+++ TestRest/Bonita_Process/conf/addNode.pl
@@ -0,0 +1,42 @@
+use strict;
+use warnings;
+use XML::Twig;
+
+
+my $reference_file;
+my $output_file ;
+
+#	Contrôle des paramètres
+my $nbparam = $#ARGV;
+if ($nbparam == 1)
+{
+	$reference_file = shift;
+	$output_file = shift;
+}
+else
+{
+	die "Nombre incorrect de paramètres \n 
+	
+	Utiliser la syntaxe suivante : \n 
+	perl $0 <fichier_reference.xml> <fichier_de_sortie.xml> \n";
+}
+																						
+#	initialisation des twigger
+my $twig_reference = new XML::Twig;
+my $twig_output = new XML::Twig;
+
+addNodeOperation();
+
+sub addNodeOperation
+{
+	#	attente de 4 min 4 x 60 secondes
+		sleep(240);
+	#	préparation de l'environnement des variables en mémoire
+		#	initialisation du twig sur le fichier de référence
+		$twig_reference -> parsefile($reference_file);
+		my $twig_reference_root = $twig_reference -> root ;
+		
+		my $twig_output_root = $twig_reference_root;
+		$twig_output_root -> insert_new_elt('enrichissement');
+		$twig_output_root -> print_to_file($output_file);
+}
